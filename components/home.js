8ea2c91206c1cdbad98aac23549ae1650cd813c7
Paul Netia <netiapaul23@gmail.com>
2022-01-06 10:31:34 +0300

    removed unwanted imports

--- components/home.js
+++ components/home.js
@@ -14,12 +14,7 @@ import {
   Divider,
   Spacer,
   Alert,
-  Avatar,
-  Button,
-  Collapse,
-  IconButton,
-  CloseIcon,
-  useToast,
+  Spinner,
 } from 'native-base';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Keychain from 'react-native-keychain';
@@ -40,6 +35,8 @@ const Dashboard = ({route, navigation}) => {
   const [points, setPoints] = useState();
   const [buy, setbuy] = useState();
   const [redeemed, setredeemed] = useState();
+  const [isloading, setIsLoading] = useState(true);
+
   useEffect(() => {
     handleSubmit();
     // console.warn('name', name);
@@ -67,6 +64,7 @@ const Dashboard = ({route, navigation}) => {
         setbuy(data[0].mempointsbuy);
         setredeemed(data[0].mempointsredeem);
         console.warn(data[0].membername);
+        setIsLoading(false);
       } else {
         // return response.json();
         // return Snackbar.show({
@@ -126,31 +124,336 @@ const Dashboard = ({route, navigation}) => {
 
   return (
     <NativeBaseProvider config={config}>
-      {status ? (
-        <Alert w="100%" status="error">
-          <VStack space={2} flexShrink={1} w="100%">
-            <HStack
-              flexShrink={1}
-              space={1}
-              alignItems="center"
-              justifyContent="space-between">
-              <HStack space={2} flexShrink={1} alignItems="center">
-                <Alert.Icon />
-                <Text
-                  fontSize="md"
-                  fontWeight="medium"
-                  _dark={{
-                    color: 'coolGray.800',
-                  }}>
-                  {status}
-                </Text>
+      <Box flex={1} bg={'#fff'}>
+        {status ? (
+          <Alert w="100%" status="error">
+            <VStack space={2} flexShrink={1} w="100%">
+              <HStack
+                flexShrink={1}
+                space={1}
+                alignItems="center"
+                justifyContent="space-between">
+                <HStack space={2} flexShrink={1} alignItems="center">
+                  <Alert.Icon />
+                  <Text
+                    fontSize="md"
+                    fontWeight="medium"
+                    _dark={{
+                      color: 'coolGray.800',
+                    }}>
+                    {status}
+                  </Text>
+                </HStack>
               </HStack>
-            </HStack>
-          </VStack>
-        </Alert>
-      ) : null}
+            </VStack>
+          </Alert>
+        ) : null}
 
-      <HStack
+        <HStack
+          shadow={2}
+          bg="#fff"
+          p="5"
+          justifyContent="space-between"
+          alignItems="center">
+          <Text color="#5d3915" fontSize="18" fontWeight="bold">
+            phAMACore Loyalty
+          </Text>
+          <Spacer />
+        </HStack>
+
+        {isloading ? (
+          <Center flex={1}>
+            <Spinner size="lg" color="warning.500" />
+          </Center>
+        ) : (
+          <>
+            <Box flex={1} bg="#fff">
+              <VStack flex={1} mt={2} bg="#fff">
+                <Box flex={2}>
+                  <HStack mx="5" mb="3">
+                    <Text
+                      color="#000"
+                      fontSize="md"
+                      style={styles.noBackGround}
+                      fontWeight="bold">
+                      Hi, {user.membername}.
+                    </Text>
+                  </HStack>
+                  <Box
+                    flex={1}
+                    // maxW="80"
+                    mx="5"
+                    mb="3"
+                    p="3"
+                    shadow={9}
+                    bg={{
+                      linearGradient: {
+                        colors: ['amber.500', 'amber.900'],
+                        start: [0, 1],
+                        end: [1, 0],
+                      },
+                    }}
+                    rounded="lg"
+                    overflow="hidden"
+                    borderColor="coolGray.200"
+                    _web={{
+                      shadow: 2,
+                      borderWidth: 0,
+                    }}>
+                    <Center flex={1}>
+                      <Heading
+                        size="md"
+                        mt={-3}
+                        fontWeight={'400'}
+                        color="#fafafa">
+                        Loyalty Points Balance
+                      </Heading>
+                    </Center>
+
+                    <Center flex={1}>
+                      <Text color="#fafafa" fontSize="3xl" fontWeight={'bold'}>
+                        {points
+                          ? points
+                              .toString()
+                              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                          : user.mempointsbal}{' '}
+                        pts
+                      </Text>
+                    </Center>
+
+                    <Center flex={1}>
+                      <HStack>
+                        <HStack>
+                          <Text color="#fafafa">Earned: </Text>
+                          <Text color="#fafafa">
+                            {buy
+                              ? buy
+                                  .toString()
+                                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                              : user.mempointsbuy}
+                          </Text>
+                        </HStack>
+                        <HStack ml={2}>
+                          <Text color="#fafafa">Redemed: </Text>
+                          <Text color="#fafafa">
+                            {redeemed
+                              ? redeemed
+                                  .toString()
+                                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                              : user.mempointsredeem}
+                          </Text>
+                        </HStack>
+                      </HStack>
+                    </Center>
+
+                    <Divider mb={2} mt={-3} />
+                    <Center justifyContent="flex-end" space={1}>
+                      <Text
+                        color="light.50"
+                        _dark={{
+                          color: 'light.100',
+                        }}
+                        fontWeight="400">
+                        Last Updated: {new Date().toDateString()}
+                      </Text>
+                    </Center>
+                  </Box>
+                </Box>
+
+                <Box
+                  bg="#fff"
+                  flex={1}
+                  mx={15}
+                  justifyContent="center"
+                  alignItems="center">
+                  <Center>
+                    <Text
+                      fontSize="2xl"
+                      fontWeight={'bold'}
+                      color="#5d3915"
+                      mb={3}
+                      mx="10">
+                      Points Summary
+                    </Text>
+                  </Center>
+
+                  <Center>
+                    <HStack>
+                      <VStack alignItems="center">
+                        <Text fontSize="sm" mx="3" mb={1} fontWeight="bold">
+                          Total Points
+                        </Text>
+                        <Text color="light.600" fontSize="sm">
+                          {points
+                            ? points
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                            : user.mempointsbal}
+                        </Text>
+                      </VStack>
+
+                      <VStack alignItems="center">
+                        <Text fontSize="sm" mx="3" mb={1} fontWeight="bold">
+                          Redeemed Points
+                        </Text>
+                        <Text color="danger.600" fontSize="sm">
+                          {redeemed
+                            ? redeemed
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                            : user.mempointsredeem}
+                        </Text>
+                      </VStack>
+
+                      <VStack alignItems="center">
+                        <Text fontSize="sm" mx="3" mb={1} fontWeight="bold">
+                          Points Buy
+                        </Text>
+                        <Text color="success.600" fontSize="sm">
+                          {buy
+                            ? buy
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                            : user.mempointsbuy}
+                        </Text>
+                      </VStack>
+                    </HStack>
+                  </Center>
+                </Box>
+
+                <VStack bg="#fff" flex={2} mx={'15'}>
+                  <Text
+                    color="#5d3915"
+                    fontSize="md"
+                    mb={5}
+                    ml={2}
+                    fontWeight={'bold'}>
+                    Activities
+                  </Text>
+
+                  <Box
+                    p="5"
+                    bg="muted.50"
+                    borderRadius="md"
+                    borderColor="coolGray.200"
+                    borderWidth="1"
+                    shadow={3}>
+                    <Link
+                      onPress={() =>
+                        navigation.navigate('transactions', {
+                          screen: 'transactions',
+                          memberNo,
+                          token,
+                        })
+                      }>
+                      <HStack>
+                        <Center>
+                          <MaterialCommunityIcons
+                            name="receipt"
+                            color={'#c58c4f'}
+                            size={30}
+                          />
+                        </Center>
+
+                        <Center>
+                          <VStack ml={2}>
+                            <Text color="#5d3915" fontWeight="bold">
+                              My Transactions History
+                            </Text>
+                          </VStack>
+                        </Center>
+                      </HStack>
+                      <Spacer />
+                      <Center>
+                        <Image
+                          source={require('../assets/images/right-arrow.png')}
+                          alt="company logo"
+                          style={styles.rightArrow}
+                          size="sm"
+                        />
+                      </Center>
+                    </Link>
+                  </Box>
+                </VStack>
+              </VStack>
+              {/* <HStack
+                shadow={5}
+                bg="#fff"
+                p="3"
+                justifyContent="space-between"
+                alignItems="center">
+                <VStack>
+                  <Center>
+                    <MaterialCommunityIcons
+                      name="home"
+                      color={'#c58c4f'}
+                      size={20}
+                    />
+                  </Center>
+
+                  <Text color="#5d3915" fontSize="10">
+                    Home
+                  </Text>
+                </VStack>
+                <Spacer />
+                <Link
+                  onPress={() =>
+                    navigation.navigate('transactions', {
+                      screen: 'transactions',
+                      memberNo,
+                      token,
+                    })
+                  }>
+                  <VStack>
+                    <Center>
+                      <MaterialCommunityIcons
+                        name="receipt"
+                        color={'#c58c4f'}
+                        size={20}
+                      />
+                    </Center>
+
+                    <Text color="#5d3915" fontSize="10">
+                      Transactions
+                    </Text>
+                  </VStack>
+                </Link>
+
+                <Spacer />
+                <Link
+                  onPress={() =>
+                    navigation.navigate('profile', {token, memberNo})
+                  }>
+                  <VStack>
+                    <Center>
+                      <MaterialCommunityIcons
+                        name="account"
+                        color={'#c58c4f'}
+                        size={20}
+                      />
+                    </Center>
+
+                    <Text color="#5d3915" fontSize="10">
+                      Profile
+                    </Text>
+                  </VStack>
+                </Link>
+              </HStack> */}
+              {/* <VStack justifyContent="flex-end" my="2">
+          <Center>
+            <Text fontSize="xs" mx="10">
+              Powered by
+            </Text>
+            <Text fontSize="xs" fontWeight="bold" color="#5d3915" mx="10">
+              Corebase Solutions
+            </Text>
+          </Center>
+        </VStack> */}
+            </Box>
+          </>
+        )}
+
+        {/* <HStack
         shadow={2}
         bg="#fff"
         p="5"
@@ -416,16 +719,75 @@ const Dashboard = ({route, navigation}) => {
             </VStack>
           </Link>
         </HStack>
-        {/* <VStack justifyContent="flex-end" my="2">
-          <Center>
-            <Text fontSize="xs" mx="10">
-              Powered by
+       // <VStack justifyContent="flex-end" my="2">
+          //  <Center>
+           //   <Text fontSize="xs" mx="10">
+           //     Powered by
+           //   </Text>
+          //    <Text fontSize="xs" fontWeight="bold" color="#5d3915" mx="10">
+          //      Corebase Solutions
+          //    </Text>
+          //  </Center>
+        //  </VStack> 
+      </Box> */}
+
+        <HStack
+          shadow={5}
+          bg="#fff"
+          p="3"
+          justifyContent="space-between"
+          alignItems="center">
+          <VStack>
+            <Center>
+              <MaterialCommunityIcons name="home" color={'#c58c4f'} size={20} />
+            </Center>
+
+            <Text color="#5d3915" fontSize="10">
+              Home
             </Text>
-            <Text fontSize="xs" fontWeight="bold" color="#5d3915" mx="10">
-              Corebase Solutions
-            </Text>
-          </Center>
-        </VStack> */}
+          </VStack>
+          <Spacer />
+          <Link
+            onPress={() =>
+              navigation.navigate('transactions', {
+                screen: 'transactions',
+                memberNo,
+                token,
+              })
+            }>
+            <VStack>
+              <Center>
+                <MaterialCommunityIcons
+                  name="receipt"
+                  color={'#c58c4f'}
+                  size={20}
+                />
+              </Center>
+
+              <Text color="#5d3915" fontSize="10">
+                Transactions
+              </Text>
+            </VStack>
+          </Link>
+
+          <Spacer />
+          <Link
+            onPress={() => navigation.navigate('profile', {token, memberNo})}>
+            <VStack>
+              <Center>
+                <MaterialCommunityIcons
+                  name="account"
+                  color={'#c58c4f'}
+                  size={20}
+                />
+              </Center>
+
+              <Text color="#5d3915" fontSize="10">
+                Profile
+              </Text>
+            </VStack>
+          </Link>
+        </HStack>
       </Box>
     </NativeBaseProvider>
   );
