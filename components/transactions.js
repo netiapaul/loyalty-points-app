5bc1b8e8ca9f26a27c85a8cccee1390ad061e271
Paul Netia <netiapaul23@gmail.com>
2022-01-06 10:31:19 +0300

    added a spinner before data is displayed and changed the fetch function to a try catch method

--- components/transactions.js
+++ components/transactions.js
@@ -14,9 +14,11 @@ import {
   Link,
   Avatar,
   Spacer,
+  Spinner,
 } from 'native-base';
 import * as Keychain from 'react-native-keychain';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Snackbar from 'react-native-snackbar';
 
 const config = {
   dependencies: {
@@ -28,249 +30,185 @@ const Transactions = ({route, navigation}) => {
   const {token, memberNo} = route.params;
   const [users, setUser] = useState([]);
   const [status, setStatus] = useState('');
+  const [isloading, setIsLoading] = useState(true);
 
   useEffect(() => {
     handleFetch();
     console.warn(users);
-    // console.warn('users', users);
-    // return () => {
-    //   cleanup
-    // }
   }, []);
 
-  const handleFetch = () => {
-    // try {
-    //   const request = `http://102.37.102.247:5016/CustomerPoints/GetCustomerTransactions?memberNo=${memberNo}`;
-    // } catch (error) {
+  async function handleFetch() {
+    try {
+      const response = await fetch(
+        `http://102.37.102.247:5016/CustomerPoints/GetCustomerTransactions?memberNo=${memberNo}`,
+        {
+          method: 'GET', // *GET, POST, PUT, DELETE, etc.
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setUser(JSON.parse(data));
+        setIsLoading(false);
+      } else {
+        return navigation.goBack();
+      }
+    } catch (error) {
+      return Snackbar.show({
+        backgroundColor: '#e11d48',
+        text: 'Network request failed connect to the internet',
+        duration: Snackbar.LENGTH_SHORT,
+      });
+    }
 
-    // }
-    const request = `http://102.37.102.247:5016/CustomerPoints/GetCustomerTransactions?memberNo=${memberNo}`;
+    // const request = `http://102.37.102.247:5016/CustomerPoints/GetCustomerTransactions?memberNo=${memberNo}`;
 
-    fetch(request, {
-      method: 'GET',
-      headers: new Headers({
-        Authorization: `Bearer ${token}`,
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      }),
-    })
-      .then(response => response.json())
-      .then(response => {
-        setUser(JSON.parse(response));
-      })
-      .catch(() => setStatus('please connect to available network'));
-  };
+    // fetch(request, {
+    //   method: 'GET',
+    //   headers: new Headers({
+    //     Authorization: `Bearer ${token}`,
+    //     Accept: 'application/json',
+    //     'Content-Type': 'application/json',
+    //   }),
+    // })
+    //   .then(response => response.json())
+    //   .then(response => {
+    //     setUser(JSON.parse(response));
+    //   })
+    //   .catch(() => setStatus('please connect to available network'));
+  }
 
   return (
-    <NativeBaseProvider config={config} bg="#fff">
-      {/* <HStack
-        bg="#fff"
-        p="5"
-        justifyContent="space-between"
-        alignItems="center">
-        <Text color="#5d3915" fontSize="20" fontWeight="bold">
-          Transactions & Points
-        </Text>
-      </HStack> */}
-
-      {status ? (
-        <Alert w="100%" status="error">
-          <VStack space={2} flexShrink={1} w="100%">
-            <HStack
-              flexShrink={1}
-              space={1}
-              alignItems="center"
-              justifyContent="space-between">
-              <HStack space={2} flexShrink={1} alignItems="center">
-                <Alert.Icon />
-                <Text
-                  fontSize="md"
-                  fontWeight="medium"
-                  _dark={{
-                    color: 'coolGray.800',
-                  }}>
-                  Connect to an internet connection
-                </Text>
-              </HStack>
-              {/* <IconButton
+    <NativeBaseProvider config={config}>
+      <Box flex={1} bg={'#fff'}>
+        {status ? (
+          <Alert w="100%" status="error">
+            <VStack space={2} flexShrink={1} w="100%">
+              <HStack
+                flexShrink={1}
+                space={1}
+                alignItems="center"
+                justifyContent="space-between">
+                <HStack space={2} flexShrink={1} alignItems="center">
+                  <Alert.Icon />
+                  <Text
+                    fontSize="md"
+                    fontWeight="medium"
+                    _dark={{
+                      color: 'coolGray.800',
+                    }}>
+                    Connect to an internet connection
+                  </Text>
+                </HStack>
+                {/* <IconButton
                   variant="unstyled"
                   icon={<CloseIcon size="3" color="coolGray.600" />}
                   onPress={() => setShow(false)}
                 /> */}
-            </HStack>
-          </VStack>
-        </Alert>
-      ) : null}
-      <Box flex={1} bg="#fff">
-        <ScrollView>
-          {/* Activity Items */}
+              </HStack>
+            </VStack>
+          </Alert>
+        ) : null}
 
-          {users.map((user, index) => {
-            return (
-              <Box
-                p="5"
-                mt={2}
-                mx="2"
-                bg="muted.50"
-                key={index}
-                borderRadius="md"
-                borderColor="coolGray.200"
-                onPress={() => console.warn('pressed')}
-                borderWidth="1"
-                shadow={1}>
-                <Link
-                  onPress={() =>
-                    navigation.navigate('transactiondetails', {
-                      token,
-                      memberNo: memberNo,
-                      docNum: user.DOCNUM,
-                      salesBCODE: user.SALESBCODE,
-                      branch: user.SALESBRANCH,
-                      gain: user.MEMPOINTSBUY,
-                      redeemed: user.MEMPOINTSREDEEM,
-                      transdate: user.SALEDATE,
-                    })
-                  }>
-                  <HStack>
-                    <VStack>
-                      <Text
-                        // color="#5d3915"
-                        color="#ff720d"
-                        fontSize={12}>
-                        {new Date(user.SALEDATE).toDateString()}
-                        {/* {new Date(user.SALEDATE).toLocaleDateString()} */}
-                      </Text>
-                      <Text color={'muted.800'} fontWeight="bold" fontSize={15}>
-                        {user.DOCNUM}
-                      </Text>
-                      <Text color={'light.600'} fontWeight="bold" fontSize={15}>
-                        {user.SALESBRANCH}
-                      </Text>
-                      {/* <Text
-                        color="#5d3915"
-                        ml={3}
-                        fontWeight="bold"
-                        fontSize={15}>
-                        Total Items Bought: {user.ItemCount}
-                      </Text>
-
-                       <HStack my={2}>
-                        <Text color="success.600" ml={3} fontWeight="bold">
-                          Received: {user.MEMPOINTSBUY}
+        {isloading ? (
+          <Center flex={1}>
+            <Spinner size="lg" color="warning.500" />
+          </Center>
+        ) : (
+          // <Box flex={1} bg="#fff">
+          <ScrollView>
+            {users.map((user, index) => {
+              return (
+                <Box
+                  p="5"
+                  mt={2}
+                  mx="2"
+                  bg="muted.50"
+                  key={index}
+                  borderRadius="md"
+                  borderColor="coolGray.200"
+                  onPress={() => console.warn('pressed')}
+                  borderWidth="1"
+                  shadow={1}>
+                  <Link
+                    onPress={() =>
+                      navigation.navigate('transactiondetails', {
+                        token,
+                        memberNo: memberNo,
+                        docNum: user.DOCNUM,
+                        salesBCODE: user.SALESBCODE,
+                        branch: user.SALESBRANCH,
+                        gain: user.MEMPOINTSBUY,
+                        redeemed: user.MEMPOINTSREDEEM,
+                        transdate: user.SALEDATE,
+                      })
+                    }>
+                    <HStack>
+                      <VStack>
+                        <Text
+                          // color="#5d3915"
+                          color="#ff720d"
+                          fontSize={12}>
+                          {new Date(user.SALEDATE).toDateString()}
                         </Text>
-                        <Text color="danger.600" ml={3} fontWeight="bold">
-                          Redeemed: {user.MEMPOINTSREDEEM}
+                        <Text
+                          color={'muted.800'}
+                          fontWeight="bold"
+                          fontSize={15}>
+                          {user.DOCNUM}
                         </Text>
-                      </HStack> */}
-                    </VStack>
-                  </HStack>
-                  <Spacer />
-                  <Center>
-                    {/* <Avatar bg="success.500" size="md">
-                      {user.SALESBRANCH.match(/\b([A-Z])/g).join('')}
-                    </Avatar> */}
-                    <Text color="success.600" ml={3} fontWeight="bold">
-                      {user.MEMPOINTSBUY.toString().replace(
-                        /\B(?=(\d{3})+(?!\d))/g,
-                        ',',
-                      )}
-                    </Text>
-                    <Text color="danger.600" ml={3} fontWeight="bold">
-                      {user.MEMPOINTSREDEEM.toString().replace(
-                        /\B(?=(\d{3})+(?!\d))/g,
-                        ',',
-                      )}
-                    </Text>
-                  </Center>
-                </Link>
-              </Box>
-            );
-          })}
-        </ScrollView>
+                        <Text
+                          color={'light.600'}
+                          fontWeight="bold"
+                          fontSize={15}>
+                          {user.SALESBRANCH}
+                        </Text>
+                      </VStack>
+                    </HStack>
+                    <Spacer />
+                    <Center>
+                      <Text color="success.600" ml={3} fontWeight="bold">
+                        {user.MEMPOINTSBUY.toString().replace(
+                          /\B(?=(\d{3})+(?!\d))/g,
+                          ',',
+                        )}
+                      </Text>
+                      <Text color="danger.600" ml={3} fontWeight="bold">
+                        {user.MEMPOINTSREDEEM.toString().replace(
+                          /\B(?=(\d{3})+(?!\d))/g,
+                          ',',
+                        )}
+                      </Text>
+                    </Center>
+                  </Link>
+                </Box>
+              );
+            })}
+          </ScrollView>
+          //  </Box>
+        )}
 
-        {/* <HStack
-          shadow={2}
-          bg="#fff"
-          p="2"
-          justifyContent="flex-end"
-          justifyContent="space-between"
-          alignItems="center">
-          <Link onPress={() => navigation.navigate('different', {token})}>
-            <VStack>
-              <Center>
-                <MaterialCommunityIcons
-                  name="home"
-                  color={'#c58c4f'}
-                  size={20}
-                />
-              </Center>
-
-              <Text color="#5d3915" fontSize="10">
-                Home
-              </Text>
-            </VStack>
-          </Link>
-
-          <Spacer />
-          <Link
-            onPress={() =>
-              navigation.navigate('transactions', {
-                screen: 'transactions',
-                memberNo,
-                token,
-              })
-            }>
-            <VStack>
-              <Center>
-                <MaterialCommunityIcons
-                  name="bank"
-                  color={'#c58c4f'}
-                  size={20}
-                />
-              </Center>
-
-              <Text color="#5d3915" fontSize="10">
-                Transactions
-              </Text>
-            </VStack>
-          </Link>
-
-          <Spacer />
-          <Link
-            onPress={() => navigation.navigate('profile', {token, memberNo})}>
-            <VStack>
-              <Center>
-                <MaterialCommunityIcons
-                  name="account"
-                  color={'#c58c4f'}
-                  size={20}
-                />
-              </Center>
-
-              <Text color="#5d3915" fontSize="10">
-                Profile
-              </Text>
-            </VStack>
-          </Link>
-        </HStack> */}
-      </Box>
-
-      <VStack flex={1} bg={'#fff'} justifyContent="flex-end">
-        <Center>
-          <Text fontSize="xs" mx="10">
-            Powered by
-          </Text>
-          <Image
-            style={styles.image}
-            source={require('../assets/images/pcico.png')}
-            alt="Company Logo"
-            size="xs"
-          />
-          {/* <Text fontSize="xs" fontWeight="bold" color="#5d3915" mx="10">
+        <VStack bg={'#fff'} justifyContent="flex-end">
+          <Center>
+            <Text fontSize="xs" mx="10">
+              Powered by
+            </Text>
+            <Image
+              style={styles.image}
+              source={require('../assets/images/pcico.png')}
+              alt="Company Logo"
+              size="xs"
+            />
+            {/* <Text fontSize="xs" fontWeight="bold" color="#5d3915" mx="10">
             Corebase Solutions
           </Text> */}
-        </Center>
-      </VStack>
+          </Center>
+        </VStack>
+      </Box>
     </NativeBaseProvider>
   );
 };
