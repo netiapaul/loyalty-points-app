2907c7b3b2bdd2b109dc395e7e9a83911c16965a
Paul Netia <netiapaul23@gmail.com>
2022-01-06 14:33:51 +0300

    can display the first 5 transaction summary

--- components/home.js
+++ components/home.js
@@ -29,6 +29,8 @@ const config = {
 
 const Dashboard = ({route, navigation}) => {
   const [user, setUser] = useState({});
+  const [transactions, setTransactions] = useState([]);
+
   const [status, setStatus] = useState('');
   const {token, memberNo} = route.params;
   const [name, setName] = useState('');
@@ -39,9 +41,40 @@ const Dashboard = ({route, navigation}) => {
 
   useEffect(() => {
     handleSubmit();
+    handleTransactions();
+    console.warn(transactions);
     // console.warn('name', name);
     // console.warn('paramater', token);
   }, []);
+
+  async function handleTransactions() {
+    try {
+      const response = await fetch(
+        `http://102.37.102.247:5016/CustomerPoints/GetCustomerTransactions?memberNo=${memberNo}`,
+        {
+          method: 'GET', // *GET, POST, PUT, DELETE, etc.
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setTransactions(JSON.parse(data));
+      } else {
+        console.error('error');
+      }
+    } catch (error) {
+      // setIsLoading(false);
+      return Snackbar.show({
+        backgroundColor: '#e11d48',
+        text: 'Network request failed connect to the internet',
+        duration: Snackbar.LENGTH_SHORT,
+      });
+    }
+  }
 
   async function GetUSerData() {
     try {
@@ -169,7 +202,7 @@ const Dashboard = ({route, navigation}) => {
           <>
             <Box flex={1} bg="#fff">
               <VStack flex={1} mt={2} bg="#fff">
-                <Box flex={2}>
+                <Box flex={1}>
                   <HStack mx="5" mb="3">
                     <Text
                       color="#000"
@@ -260,67 +293,6 @@ const Dashboard = ({route, navigation}) => {
                   </Box>
                 </Box>
 
-                <Box
-                  bg="#fff"
-                  flex={1}
-                  mx={15}
-                  justifyContent="center"
-                  alignItems="center">
-                  <Center>
-                    <Text
-                      fontSize="2xl"
-                      fontWeight={'bold'}
-                      color="#5d3915"
-                      mb={3}
-                      mx="10">
-                      Points Summary
-                    </Text>
-                  </Center>
-
-                  <Center>
-                    <HStack>
-                      <VStack alignItems="center">
-                        <Text fontSize="sm" mx="3" mb={1} fontWeight="bold">
-                          Total Points
-                        </Text>
-                        <Text color="light.600" fontSize="sm">
-                          {points
-                            ? points
-                                .toString()
-                                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                            : user.mempointsbal}
-                        </Text>
-                      </VStack>
-
-                      <VStack alignItems="center">
-                        <Text fontSize="sm" mx="3" mb={1} fontWeight="bold">
-                          Redeemed Points
-                        </Text>
-                        <Text color="danger.600" fontSize="sm">
-                          {redeemed
-                            ? redeemed
-                                .toString()
-                                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                            : user.mempointsredeem}
-                        </Text>
-                      </VStack>
-
-                      <VStack alignItems="center">
-                        <Text fontSize="sm" mx="3" mb={1} fontWeight="bold">
-                          Points Buy
-                        </Text>
-                        <Text color="success.600" fontSize="sm">
-                          {buy
-                            ? buy
-                                .toString()
-                                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                            : user.mempointsbuy}
-                        </Text>
-                      </VStack>
-                    </HStack>
-                  </Center>
-                </Box>
-
                 <VStack bg="#fff" flex={2} mx={'15'}>
                   <Text
                     color="#5d3915"
@@ -330,6 +302,98 @@ const Dashboard = ({route, navigation}) => {
                     fontWeight={'bold'}>
                     Activities
                   </Text>
+
+                  <Box
+                    p="1"
+                    mx="1"
+                    bg="muted.50"
+                    // bg="#000"
+                    // borderRadius="sm"
+                    mb={5}
+                    borderColor="coolGray.200"
+                    // shadow={1}
+                    borderWidth="1">
+                    <Text fontWeight="500" fontSize={12} my={1}>
+                      Last 5 Transactions:
+                    </Text>
+                    <Divider my={1} />
+                    {transactions.slice(0, 5).map((transaction, index) => {
+                      return (
+                        <Box p={1}>
+                          <HStack>
+                            <VStack>
+                              <Text
+                                color={'muted.800'}
+                                fontWeight="bold"
+                                fontSize={15}>
+                                {transaction.DOCNUM}
+                              </Text>
+                              <HStack>
+                                <Text
+                                  color={'muted.800'}
+                                  fontWeight="400"
+                                  fontSize={10}>
+                                  {new Date(
+                                    transaction.SALEDATE,
+                                  ).toDateString()}
+                                </Text>
+                                <Center>
+                                  <Divider
+                                    orientation="vertical"
+                                    h={3}
+                                    bg={'muted.500'}
+                                    mx={1}
+                                  />
+                                </Center>
+
+                                <Text
+                                  color={'muted.800'}
+                                  fontWeight="400"
+                                  fontSize={10}>
+                                  {transaction.SALESBRANCH}
+                                </Text>
+                              </HStack>
+                            </VStack>
+
+                            <Spacer />
+                            <VStack>
+                              <Text
+                                color={'muted.800'}
+                                fontWeight="bold"
+                                fontSize={15}>
+                                Kshs.
+                                {transaction.DOCNUM}
+                              </Text>
+                              <HStack space={3}>
+                                <Text
+                                  color={'muted.800'}
+                                  fontWeight="400"
+                                  fontSize={10}>
+                                  EPts:{' '}
+                                  {transaction.MEMPOINTSBUY.toString().replace(
+                                    /\B(?=(\d{3})+(?!\d))/g,
+                                    ',',
+                                  )}
+                                </Text>
+
+                                <Text
+                                  color={'muted.800'}
+                                  fontWeight="400"
+                                  fontSize={10}>
+                                  RPts:{' '}
+                                  {transaction.MEMPOINTSREDEEM.toString().replace(
+                                    /\B(?=(\d{3})+(?!\d))/g,
+                                    ',',
+                                  )}
+                                </Text>
+                              </HStack>
+                            </VStack>
+                          </HStack>
+                          <Divider my={1} />
+                        </Box>
+                      );
+                    })}
+                  </Box>
 
                   <Box
                     p="5"
@@ -358,7 +422,7 @@ const Dashboard = ({route, navigation}) => {
                         <Center>
                           <VStack ml={2}>
                             <Text color="#5d3915" fontWeight="bold">
-                              My Transactions History
+                              My Transactions
                             </Text>
                           </VStack>
                         </Center>
