6e075fdaf77f38485f726d3d233668aa2ba67289
Paul Netia <netiapaul23@gmail.com>
2022-01-06 12:54:39 +0300

    added a setTimeout to delay the splash screen

--- index.js
+++ index.js
@@ -7,13 +7,23 @@ import {name as appName} from './app.json';
 import SplashScreen from 'react-native-splash-screen';
 
 AppRegistry.registerComponent(appName, () => {
-  (function () {
+  // (function () {
+  //   setTimeout(() => {
+  //     console.log('app running');
+  //     console.log('app running');
+  //     console.log('app running');
+  //     console.log('app running');
+  //     console.log('app running');
+  //   }, 6000);
+  // })();
+  setTimeout(() => {
     console.log('app running');
     console.log('app running');
     console.log('app running');
     console.log('app running');
     console.log('app running');
-  })();
+  }, 6000);
+  clearInterval(setTimeout);
   SplashScreen.hide();
   return App;
 });
